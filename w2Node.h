6f1d2f491a1bf1bfe9c197eaa0a429e76011c010
julia57 <semavina57@yandex.ru>
2017-05-22 19:12:10 +0300

    These are week2 solutions
    files w2BinaryTree.* and w2Node.h are for both problems
    file w2q1main.cpp is the main file for q1
    file w2q2main.cpp is the main file for q2

---
+++ w2Node.h
@@ -0,0 +1,18 @@
+
+//
+// Created by julia on 5/22/17.
+//
+
+#ifndef TRY2_NODE_H
+#define TRY2_NODE_H
+
+
+struct Node {
+    int name;
+    Node* parent;
+    Node* left;
+    Node* right;
+};
+
+
+#endif //TRY2_NODE_H
